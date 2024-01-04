6e0a2ba94af842be6cdb9ffb093a7a338034d3ae
jkyoon <jkyoon.dev@gmail.com>
2024-01-04 21:44:26 +0900

    Row 스크롤 처리 SingleChildScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -15,43 +15,65 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
+    // 스크롤
+    return SingleChildScrollView(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-
-            /// children 복수형으로 사용
-            children: [
-              Container(
-                color: Colors.red,
-                width: 100,
-                height: 80,
-                child: Text('Container 1'),
-              ),
-              Container(
-                color: Colors.orange,
-                width: 100,
-                height: 80,
-                child: Text('Container 2'),
-              ),
-              Container(
-                color: Colors.yellow,
-                width: 100,
-                height: 80,
-                child: Text('Container 3'),
-              ),
-            ],
+          Container(
+            color: Colors.grey,
+            width: double.infinity,
+            height: 100,
+            margin: EdgeInsets.symmetric(vertical: 8),
           ),
           Container(
-            color: Colors.green,
-            width: 300,
-            height: 120,
-            child: Text('Container 4'),
+            color: Colors.grey,
+            width: double.infinity,
+            height: 100,
+            margin: EdgeInsets.symmetric(vertical: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: double.infinity,
+            height: 100,
+            margin: EdgeInsets.symmetric(vertical: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: double.infinity,
+            height: 100,
+            margin: EdgeInsets.symmetric(vertical: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: double.infinity,
+            height: 100,
+            margin: EdgeInsets.symmetric(vertical: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: double.infinity,
+            height: 100,
+            margin: EdgeInsets.symmetric(vertical: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: double.infinity,
+            height: 100,
+            margin: EdgeInsets.symmetric(vertical: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: double.infinity,
+            height: 100,
+            margin: EdgeInsets.symmetric(vertical: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: double.infinity,
+            height: 100,
+            margin: EdgeInsets.symmetric(vertical: 8),
           ),
         ],
       ),
