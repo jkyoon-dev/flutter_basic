38eb23cb902b0cc2c25cb4aa3b0083d218c1e4b6
jkyoon <jkyoon.dev@gmail.com>
2024-01-04 23:48:00 +0900

    Stateless,Stateful 비교

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,85 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home: HomeWidget(),
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(),
+      body: body(),
     ),
-  );
+  ));
 }
 
-class HomeWidget extends StatelessWidget {
-  const HomeWidget({super.key});
+class body extends StatelessWidget {
+  const body({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: ConstraintsWidget(),
+    return Column(
+      children: [
+        ExampleStateless(),
+        ExampleStateful(),
+      ],
+    );
+  }
+}
+
+class ExampleStateless extends StatelessWidget {
+  const ExampleStateless({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        color: Colors.red,
       ),
     );
   }
 }
 
-class ConstraintsWidget extends StatelessWidget {
-  const ConstraintsWidget({super.key});
+class ExampleStateful extends StatefulWidget {
+  const ExampleStateful({super.key});
+
+  @override
+  State<ExampleStateful> createState() => _ExampleStatefulState();
+}
+
+class _ExampleStatefulState extends State<ExampleStateful> {
+  // 모든 변수는 상태객체에 넣어줘야된다.
+  // int index = 0;
+  late int index;
+  late TextEditingController textController;
+  @override
+  void initState() {
+    // 부모까지 적용하려면
+    super.initState();
+    index = 5;
+    textController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    textController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 500,
-      width: 500,
-      color: Colors.blue,
-      // Align => 많이씀, SingleChildScrollView => overflow
-      child: SingleChildScrollView(
+    return Expanded(
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            if (index == 5) {
+              index = 0;
+              return;
+            }
+            index++;
+            print(index);
+          });
+        },
         child: Container(
-          color: Colors.red,
-          height: 300000,
-          width: 300,
-          child: Container(
-            // 제약조건으로 position relative 느낌
-            constraints: BoxConstraints.tight(
-              Size(200, 200),
-            ),
-            margin: EdgeInsets.all(20),
+          color: Colors.blue.withOpacity(index / 5),
+          child: Center(
+            child: Text('$index'),
           ),
         ),
       ),
