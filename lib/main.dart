2c0c3e57cd60021c897e7f37eebbe53e103716e5
윤종경 <jkyoon@danawa.com>
2024-01-04 14:15:41 +0900

    flutter init

--- lib/main.dart
+++ lib/main.dart
@@ -3,35 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          actions: [
-            IconButton(
-              icon: Icon(Icons.home),
-              onPressed: () {
-                print('tab');
-              },
-            ),
-            Icon(Icons.play_arrow),
-          ],
-          centerTitle: true,
-          title: Text('this is AppBar'),
-        ),
-        body: TestWidget(),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.bug_report),
-          onPressed: () => print('Tab FAB!'),
-        ),
-      ),
+      home: Scaffold(),
     ),
   );
-}
-
-class TestWidget extends StatelessWidget {
-  const TestWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Text('Hello Flutter');
-  }
 }
